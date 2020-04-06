53aacd0931b52027205c98a505bce6884e9db730
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-06 14:41:45 +0430

    blog post type init

---
+++ src/containers/blog-post/blog-post.types.ts
@@ -0,0 +1,35 @@
+interface IBlogPostBase {
+    /**
+     * image url(s) of blog post
+     */
+    imageUrl: string[] | string;
+
+    /**
+     * title of blog post
+     */
+    title: string | React.ReactNode;
+
+    /**
+     * short description of blog post
+     */
+    shortDesc: string;
+
+    /**
+     * complete body of blog post
+     */
+    body: string;
+
+    /**
+     * post release data
+     */
+    date: Date;
+
+    /**
+     * blog post page  url
+     */
+    url: string;
+}
+
+export interface IBlogPost
+    extends Omit<React.HtmlHTMLAttributes<HTMLDivElement | HTMLSpanElement>, keyof IBlogPostBase>,
+        IBlogPostBase {}
