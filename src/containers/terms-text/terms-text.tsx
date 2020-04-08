967edf544bfb2eb9c54a822d5b23b5d8f694e04e
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-08 10:52:24 +0430

    terms text init

---
+++ src/containers/terms-text/terms-text.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./terms-text.scss";
+
+export const TermsText = (props: {}) => {
+    return (
+        <div className="terms">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+        </div>
+    );
+};
