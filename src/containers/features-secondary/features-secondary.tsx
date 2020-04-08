ba56030976aa4f9e9df03b2046c0b015102c30d0
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-08 20:05:55 +0430

    features secondary container added

---
+++ src/containers/features-secondary/features-secondary.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Card } from "components/Card";
+import "./features-secondary.scss";
+
+export const FeaturesSecondary = (props: {}) => {
+    return (
+        <div>
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <Card>
+                            <div className="features">
+                                <div className="features__content">
+                                    <div className="features__title">ارسال رایگان کالا ها</div>
+                                    <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
+                                </div>
+                                <img className="features__image" src="/assets/images/icons/packaging.svg" alt="image" />
+                            </div>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-4">
+                        <Card>
+                            <div className="features">
+                                <div className="features__content">
+                                    <div className="features__title">ارسال رایگان کالا ها</div>
+                                    <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
+                                </div>
+                                <img
+                                    className="features__image"
+                                    src="/assets/images/icons/delivery-truck.svg"
+                                    alt="image"
+                                />
+                            </div>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-4">
+                        <Card>
+                            <div className="features">
+                                <div className="features__content">
+                                    <div className="features__title">ارسال رایگان کالا ها</div>
+                                    <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
+                                </div>
+
+                                <img className="features__image" src="/assets/images/icons/shop.svg" alt="image" />
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
