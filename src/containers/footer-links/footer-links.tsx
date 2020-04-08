e0401e4086c4435326c165d9be5b46127b08c203
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-08 10:52:43 +0430

    footer links init

---
+++ src/containers/footer-links/footer-links.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./footer-links.scss";
+
+export const FooterLinks = (props: {}) => {
+    return (
+        <div className="links">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <div className="links__title">دسترسی سریع</div>
+                        <div className="links__item">دسترسی سریع</div>
+                        <div className="links__item">دسترسی سریع</div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <div className="links__title">دسترسی سریع</div>
+                        <div className="links__item">دسترسی سریع</div>
+                        <div className="links__item">دسترسی سریع</div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <div className="links__title">دسترسی سریع</div>
+                        <div className="links__item">دسترسی سریع</div>
+                        <div className="links__item">دسترسی سریع</div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-3">
+                        <div className="links__text">
+                            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                            چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                            تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در
+                            شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها
+                            شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی
+                            ایجاد کرد.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
