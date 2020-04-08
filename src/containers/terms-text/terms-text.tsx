0434c8b5781bcce92ebec634fc9afa84f098c0fb
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-08 11:44:15 +0430

    terms text grid updated

--- src/containers/terms-text/terms-text.tsx
+++ src/containers/terms-text/terms-text.tsx
@@ -4,7 +4,13 @@ import "./terms-text.scss";
 export const TermsText = (props: {}) => {
     return (
         <div className="terms">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col">
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
