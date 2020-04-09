a70eb4fa24b9db2bc5c723dcfe38bbd1bf4a4f81
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-09 13:46:32 +0430

    main pages init

---
+++ src/pages/Blog/blog-page.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useCallback } from "react";
+import { MainLayout } from "containers/main-layout";
+import { SearchCard } from "containers/search-card";
+import { BlogPost } from "containers/blog-post";
+import { BlogPost as BlogPostData } from "data/BlogPost";
+export function BlogPage() {
+    const [filter, setFilter] = useState("");
+    const [posts, setPosts] = useState(BlogPostData);
+
+    const changeFilter = useCallback(
+        (value) => {
+            if (value === "") {
+                setPosts(BlogPostData);
+            } else {
+                setFilter(value);
+                setPosts(BlogPostData.filter((item) => item.title.includes(filter)));
+            }
+        },
+        [filter]
+    );
+
+    return (
+        <>
+            <MainLayout>
+                <div className="row">
+                    <div className="col-12 col-lg-3">
+                        <SearchCard onChange={(e) => changeFilter(e.target.value)} />
+                    </div>
+                    <div className="col-12 col-lg-9 d-flex flex-row flex-wrap">
+                        {posts.map((item) => (
+                            <div className="col-12 col-lg-6 p-0 px-lg-2">
+                                <BlogPost title={item.title} shortDesc={item.shortDesc} linkText="بیشتر بخوانید" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </MainLayout>
+        </>
+    );
+}
---
+++ src/pages/Brands/brands-page.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { MainLayout } from "containers/main-layout";
+import { BrandCard } from "containers/brand-card/brand-card";
+export function BrandsPage() {
+    return (
+        <MainLayout>
+            <div className="row">
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/diesel.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/gucci.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/adidas.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/lacoste.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/diesel.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/gucci.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/adidas.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/lacoste.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/diesel.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/gucci.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/adidas.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/lacoste.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/diesel.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/gucci.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/adidas.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/lacoste.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/diesel.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/gucci.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/adidas.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/lacoste.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+                <div className="col-6 col-md-4 col-lg-2 mb-2">
+                    <BrandCard image="/assets/images/icons/brands/puma.svg" name="gucci" />
+                </div>
+            </div>
+        </MainLayout>
+    );
+}
