f840bf6123bfe6f52ad6e017a301d6248303afac
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-09 22:42:02 +0430

    features secondary alignment update

--- src/containers/features-secondary/features-secondary.tsx
+++ src/containers/features-secondary/features-secondary.tsx
@@ -4,48 +4,40 @@ import "./features-secondary.scss";
 
 export const FeaturesSecondary = (props: {}) => {
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-4">
-                        <Card>
-                            <div className="features">
-                                <div className="features__content">
-                                    <div className="features__title">ارسال رایگان کالا ها</div>
-                                    <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
-                                </div>
-                                <img className="features__image" src="/assets/images/icons/packaging.svg" alt="image" />
-                            </div>
-                        </Card>
+        <div className="row my-4">
+            <div className="col-12 col-md-4">
+                <Card>
+                    <div className="features">
+                        <div className="features__content">
+                            <div className="features__title">ارسال رایگان کالا ها</div>
+                            <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
+                        </div>
+                        <img className="features__image" src="/assets/images/icons/packaging.svg" alt="image" />
                     </div>
-                    <div className="col-12 col-md-4">
-                        <Card>
-                            <div className="features">
-                                <div className="features__content">
-                                    <div className="features__title">ارسال رایگان کالا ها</div>
-                                    <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
-                                </div>
-                                <img
-                                    className="features__image"
-                                    src="/assets/images/icons/delivery-truck.svg"
-                                    alt="image"
-                                />
-                            </div>
-                        </Card>
+                </Card>
+            </div>
+            <div className="col-12 col-md-4">
+                <Card>
+                    <div className="features">
+                        <div className="features__content">
+                            <div className="features__title">ارسال رایگان کالا ها</div>
+                            <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
+                        </div>
+                        <img className="features__image" src="/assets/images/icons/delivery-truck.svg" alt="image" />
                     </div>
-                    <div className="col-12 col-md-4">
-                        <Card>
-                            <div className="features">
-                                <div className="features__content">
-                                    <div className="features__title">ارسال رایگان کالا ها</div>
-                                    <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
-                                </div>
+                </Card>
+            </div>
+            <div className="col-12 col-md-4">
+                <Card>
+                    <div className="features">
+                        <div className="features__content">
+                            <div className="features__title">ارسال رایگان کالا ها</div>
+                            <div className="features__description">برای خرید های بالای 100 هزار تومان</div>
+                        </div>
 
-                                <img className="features__image" src="/assets/images/icons/shop.svg" alt="image" />
-                            </div>
-                        </Card>
+                        <img className="features__image" src="/assets/images/icons/shop.svg" alt="image" />
                     </div>
-                </div>
+                </Card>
             </div>
         </div>
     );
