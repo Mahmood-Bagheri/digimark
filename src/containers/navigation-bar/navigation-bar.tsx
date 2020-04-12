1860615be64b64b06d6633c92b70cf209676c8ac
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-12 22:28:47 +0430

    add link to navbar items

--- src/containers/navigation-bar/navigation-bar.tsx
+++ src/containers/navigation-bar/navigation-bar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./navigation-bar.scss";
+import { NavLink } from "react-router-dom";
 interface Props {}
 
 export const Navbar = (props: Props) => {
@@ -7,12 +8,26 @@ export const Navbar = (props: Props) => {
         <div>
             <div className="container-fluid">
                 <div className="navbar flex-nowrap">
-                    <div className="navbar__item col-auto">فروشگاه ها</div>
-                    <div className="navbar__item col-auto">محصولات</div>
-                    <div className="navbar__item col-auto navbar__item--active">برند ها</div>
+                    <NavLink className="navbar__item col-auto" activeClassName="navbar__item--active" to="/" exact>
+                        فروشگاه ها
+                    </NavLink>
+                    <NavLink className="navbar__item col-auto" activeClassName="navbar__item--active" to="/store" exact>
+                        محصولات
+                    </NavLink>
+                    <NavLink
+                        className="navbar__item col-auto"
+                        activeClassName="navbar__item--active"
+                        to="/brands"
+                        exact
+                    >
+                        برند ها
+                    </NavLink>
+
                     <div className="navbar__item col-auto">سفارشات</div>
                     <div className="navbar__item col-auto">همکاری با ما</div>
-                    <div className="navbar__item col-auto">درباره ما</div>
+                    <NavLink className="navbar__item col-auto" activeClassName="navbar__item--active" to="/about">
+                        درباره ما
+                    </NavLink>
                 </div>
             </div>
         </div>
