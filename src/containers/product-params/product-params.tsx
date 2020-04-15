6a4e1c6ab972c13d96a421aab8a6d8c316207830
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-15 11:54:51 +0430

    product params init

---
+++ src/containers/product-params/product-params.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./product-params.scss";
+
+interface Props {}
+
+export const ProductParams = (props: Props) => {
+    return (
+        <div className="product-params">
+            <div className="product-params__title">مشخصات فنی</div>
+
+            <div className="product-params__description">لورم ایپسوم متن ساختگی با تولید سادگی</div>
+
+            <div className="product-params__container">
+                <div className="product-params__key">متن ساختگی</div>
+                <div className="product-params__value">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                </div>
+            </div>
+            <div className="product-params__container">
+                <div className="product-params__key">متن ساختگی</div>
+                <div className="product-params__value">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                </div>
+            </div>
+            <div className="product-params__container">
+                <div className="product-params__key">متن ساختگی</div>
+                <div className="product-params__value">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                </div>
+            </div>
+            <div className="product-params__container">
+                <div className="product-params__key">متن ساختگی</div>
+                <div className="product-params__value">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                </div>
+            </div>
+            <div className="product-params__container">
+                <div className="product-params__key">متن ساختگی</div>
+                <div className="product-params__value">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                </div>
+            </div>
+        </div>
+    );
+};
