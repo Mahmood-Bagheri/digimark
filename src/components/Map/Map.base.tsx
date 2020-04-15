8b04f3df95be14a9c1975a99cc0954a6f6b529ae
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-15 18:44:20 +0430

    tile updated

--- src/components/Map/Map.base.tsx
+++ src/components/Map/Map.base.tsx
@@ -13,7 +13,7 @@ export const MapControl = (props: Props) => {
         <Map className="leaflet-map" zoom={13} center={center} zoomControl={false}>
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
             />
             <Marker position={center} icon={MarkerIcon}></Marker>
         </Map>
