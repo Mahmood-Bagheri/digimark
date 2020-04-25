7cef40b6b988f99278877c7b5be209b916f8f205
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-26 01:07:49 +0430

    save search in results to state

--- src/containers/search-in-results/search-in-results.tsx
+++ src/containers/search-in-results/search-in-results.tsx
@@ -1,13 +1,20 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, ChangeEvent } from "react";
 import { Category } from "components/Category";
 import { Card } from "components/Card";
 import { Textfield } from "components/Textfield";
-
-export const SearchInResults: FunctionComponent = (props) => {
+interface IProps {
+    setSearchWord: (text: string) => void;
+}
+export const SearchInResults: FunctionComponent<IProps> = (props) => {
+    const { setSearchWord } = props;
     return (
         <div>
             <Card title="جست و جو در نتایج">
-                <Textfield label="جستجو" placeholder="نام کالا، برند و یا ..." onChange={(e) => console.log(e)} />
+                <Textfield
+                    label="جستجو"
+                    placeholder="نام کالا، برند و یا ..."
+                    onChange={(e) => setSearchWord(e.target.value)}
+                />
 
                 <div className="categories">
                     <Category id={1} text="لورم متن" getId={(e) => alert(e)} />
