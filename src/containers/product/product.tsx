a840541d58f74a0dd2203c9173c2c1e65ca78752
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-26 01:06:53 +0430

    images of product lazy loaded

--- src/containers/product/product.tsx
+++ src/containers/product/product.tsx
@@ -1,14 +1,15 @@
 import React, { FunctionComponent } from "react";
 import { IProduct } from "./product.types";
+import { Image } from "components/Image";
 import classnames from "classnames";
 import "./product.scss";
 
-export const Product: FunctionComponent<IProduct> = (props) => {
+export const Product: FunctionComponent<IProduct> = props => {
     const { title, price, image, className, ...restProps } = props;
 
     return (
         <div className={classnames("product", className)} {...restProps}>
-            <img className="product__image" src={image} alt={title as string} />
+            <Image className="product__image" src={image} alt={title as string} />
             <div className="product__content">
                 <div className="product__title">{title}</div>
                 <div className="product__price">{price} تومان</div>
