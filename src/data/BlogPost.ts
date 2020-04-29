3c808f0940077807976a02a70264e5811a33fd48
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-29 11:38:05 +0430

    import image of blog posts

--- src/data/BlogPost.ts
+++ src/data/BlogPost.ts
@@ -1,3 +1,5 @@
+import Image1 from "assets/images/image.png";
+
 export const BlogPost = [
     {
         id: 1,
@@ -5,7 +7,7 @@ export const BlogPost = [
         shortDesc:
             "ن گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای",
         date: new Date(),
-        imageUrl: "",
+        imageUrl: Image1,
         body: "body",
         url: "google.com",
         linkText: "مشاهده بیشتر",
