6d2910dda8980a393fcd94cc2b801b32bb7a1bb0
Mahmood Bagheri <mahmood.bagheri666@gmail.com>
2020-04-29 11:38:13 +0430

    routes imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React, { Suspense } from "react";
+import React, { Suspense, lazy } from "react";
 import { MainLayout } from "containers/main-layout";
 // import { BrandProducts } from "pages/Brand-Products";
 import { Route, Switch } from "react-router-dom";
 // import { ProductPage } from "pages/Product";
 import { Loading } from "components/Loading";
 
-const BrandProducts = React.lazy(() =>
-    import("pages/Brand-Products").then(({ BrandProducts }) => ({ default: BrandProducts }))
-);
+const About = lazy(() => import("pages/About"));
 
-const ProductPage = React.lazy(() => import("pages/Product").then(({ ProductPage }) => ({ default: ProductPage })));
-const BlogPage = React.lazy(() => import("pages/Blog").then(({ BlogPage }) => ({ default: BlogPage })));
+const Blog = lazy(() => import("pages/Blog"));
+
+const BrandProducts = lazy(() => import("pages/Brand-Products"));
+
+const Brands = lazy(() => import("pages/Brands"));
+
+const Contact = lazy(() => import("pages/Contact"));
+
+const Faq = lazy(() => import("pages/Faq"));
+
+const Landing = lazy(() => import("pages/Landing"));
+
+const Login = lazy(() => import("pages/Login"));
+
+const Store = lazy(() => import("pages/Store"));
+
+const Product = lazy(() => import("pages/Product"));
 
 function App() {
     return (
@@ -18,9 +31,17 @@ function App() {
             <MainLayout>
                 <Suspense fallback={<Loading />}>
                     <Switch>
-                        <Route path="/products" exact component={BrandProducts} />
-                        <Route path="/blog" exact component={BlogPage} />
-                        <Route path="/products/:id" exact component={ProductPage} />
+                        <Route path="/" exact component={Landing} />
+                        <Route path="/products" exact component={Store} />
+                        <Route path="/products/:id" exact component={Product} />
+                        <Route path="/blog" exact component={Blog} />
+                        <Route path="/contact" exact component={Contact} />
+                        <Route path="/about" exact component={About} />
+                        <Route path="/brands" exact component={Brands} />
+                        <Route path="/brands/:id" exact component={BrandProducts} />
+                        <Route path="/contact" exact component={Contact} />
+                        <Route path="/faq" exact component={Faq} />
+                        <Route path="/login" exact component={Login} />
                     </Switch>
                 </Suspense>
             </MainLayout>
